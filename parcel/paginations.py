058f1650e60c8695f0daec3ebfb04232fce62464
Yazan Shakhshir <yazan.shakhshir@hotmail.com>
2024-11-08 20:03:07 +0200

    added page_size query parameter name in pagination class

--- parcel/paginations.py
+++ parcel/paginations.py
@@ -3,4 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 class ParcelsPagination(PageNumberPagination):
     page_size = 3
     page_query_param = 'page_number'
+    page_size_query_param = 'page_size'
     max_page_size = 100
