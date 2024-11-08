ff1529f9f4329c03710de4878c83bcfa8ebe0453
Yazan Shakhshir <yazan.shakhshir@hotmail.com>
2024-11-08 17:03:07 +0200

    defined parcels model

--- parcel/models.py
+++ parcel/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class LandUseGroup(models.TextChoices):
+    AGRICULTURAL = 'Agricultural'
+    RESIDENTIAL = 'Residential'
+    COMMERCIAL = 'Commercial'
+
+
+class Parcel(models.Model):
+    block_number = models.IntegerField()
+    neighberhood = models.TextField()
+    subdivision_number = models.IntegerField()
+    land_use_group = models.CharField(choices=LandUseGroup.choices)
+    description = models.CharField()
+    creation_date = models.DateField(auto_now_add=True)
