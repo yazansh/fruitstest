8de6077d2519fc479c8fa872c35ed89d964e9f37
Yazan Shakhshir <yazan.shakhshir@hotmail.com>
2024-11-08 20:47:51 +0200

    defined broker model

--- broker/models.py
+++ broker/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
 
+
 # Create your models here.
+
+class BrokerType(models.TextChoices):
+    PERSONAL = 'Personal'
+    COMPANY = 'Company'
+    GOVERNMENTAL = 'Governmental'
+
+class Broker(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    type = models.CharField(max_length=50, choices=BrokerType.choices)
+    phone_number = models.CharField(max_length=25, default='', blank=True)
+    email = models.CharField(max_length=100, blank=False)
+    address = models.TextField(max_length=500, default='', blank=True)
+    bio = models.CharField(max_length=200, default='', blank=True)
+    creation_date = models.DateTimeField()
+    last_updated_date = models.DateTimeField()
