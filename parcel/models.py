9487058b69bb4a8cb5b5ed67cc2f9b2f1abe72cf
Yazan Shakhshir <yazan.shakhshir@hotmail.com>
2024-11-08 20:35:09 +0200

    update model to make desciption and neighborhood as optional with allow blank value

--- parcel/models.py
+++ parcel/models.py
@@ -10,8 +10,8 @@ class LandUseGroup(models.TextChoices):
 
 class Parcel(models.Model):
     block_number = models.IntegerField()
-    neighborhood = models.TextField(max_length=1000)
+    neighborhood = models.TextField(max_length=1000, default='', blank=True)
     subdivision_number = models.IntegerField()
     land_use_group = models.CharField(max_length=100, choices=LandUseGroup.choices)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, default='', blank=True)
     creation_date = models.DateField(auto_now_add=True)
